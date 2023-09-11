870eb003dc7abba99b4d284248601db67a48d930
CalebeKun <105517982+CalebeKun@users.noreply.github.com>
2023-09-11 10:50:59 -0300

    Update script.js

--- SCRIPT/script.js
+++ SCRIPT/script.js
@@ -1,3 +1,19 @@
 function Ola_Mundo(){
     alert ("Projeto Iniciando, You Are G.A.Y!!!!!")
 }
+
+escrever = msg => alert(msg);
+soma = (a,b) => a + b; 
+sub = (a,b) => a - b;
+mult = (a,b) => a * b; 
+div = (a,b) => a / b;
+raiz = a => Math.sqrt(a);
+equacao2grau = (a,b,c) => {
+    let delta = sub(mult(b,b),mult(4,mult(a,c)));
+    if (delta < 0) return "Não possui raiz Real "
+    if (delta == 0) return "x1 = x2 = " + div(-b,mult(2,a));
+    return "x1 = " + div(soma(-b,raiz(delta)),mult(2,a)) + 
+            "x2 = " + div(sub(-b,raiz(delta)),mult(2,a));
+}
+
+escrever(equacao2grau(2,3,5));

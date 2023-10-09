609dbca0008764c0e8a48100e8f44f23a33723d3
CalebeKun <105517982+CalebeKun@users.noreply.github.com>
2023-10-09 09:13:17 -0300

    Update script.js

--- SCRIPT/script.js
+++ SCRIPT/script.js
@@ -1,41 +1,95 @@
-let a = "";
-let b = "";
-let valor = 0;
+escrever = msg => alert(msg);
+let a = 0;
+let b = 0;
+let valor = "";
 let executar = "";
-
-soma = (a,b) => a + b;
-sub = (a,b) => a - b;
-mult = (a,b) => a * b;
-div = (a,b) => a / b;
+let temPonto = false;
+let desligada = false;
+soma = (a, b) => Number(a) + Number(b);
+sub = (a, b) => Number(a) - Number(b);
+mult = (a, b) => Number(a) * Number(b);
+div = (a, b) => Number(a) / Number(b);
 raiz = a => Math.sqrt(a);
-equacao2Grau = (a,b,c) =>{
-    let delta = sub(mult(b,b),mult(4,mult(a,c)));
-    if (delta < 0) return "Não possui raiz Real.";
-    if (delta == 0) return "x1 = x2 = " + div(-b,mult(2,a));
-    return "x1 = " + div(soma(-b,raiz(delta)),mult(2,a)) + 
-           " x2 = " + div(sub(-b,raiz(delta)),mult(2,a));
+
+equacao2Grau = (a, b, c) => {
+    let delta = sub(mult(b, b), mult(4, mult(a, c)));
+    if (delta < 0) return "Não possui raiz real.";
+    if (delta == 0) return "x1 = x2 = " + div(-b, mult(2, a));
+
+    return "x1 = " + div(soma(-b, raiz(delta)), mult(2, a)) +
+        " x2 = " + div(sub(-b, raiz(delta)), mult(2, a));
 }
-function mostrar_resultado(){
+
+function mostrar_resultado() {
     document.getElementById("resultado").value = valor;
 }
-function calcular(){
-    if(executar != ""){
-        if(executar = "soma") mostrar_resultado(soma(a,b));
-        if(executar = "sub") mostrar_resultado(sub(a,b));
-        if(executar = "div") mostrar_resultado(div(a,b));
-        if(executar = "mult") mostrar_resultado(mult(a,b));
-        executar = "";
-        a = ""
+
+function calcular() {
+    if (executar != "") {
+        b = valor;
+        if (executar == "soma") valor = soma(a,b);
+        if (executar == "sub") valor = sub(a,b);
+        if (executar == "div") valor = div(a,b);
+        if (executar == "mult") valor = mult(a,b);
+        if (executar == "porc") valor = mult((div(a,100)),b);
+        if (executar == "raiz") valor = raiz(a);
+        mostrar_resultado();
+        executar = ""
+        a = "";
         b = "";
+        valor = "";
     }
 }
-function digitando(tecla){
-   if(executar == ""){
-       a += tecla;
-       alert(a);
-   }else{
-       b += tecla;
-   } 
-   
 
+function desliga(){
+    desligada = !desligada;
+    if(desligada){
+        zerar();
+    } else{
+        zerar()
+        mostrar_resultado();
+    }
+    return desligada;
+}
+
+desliga();
+
+function calcula_raiz(){
+    if(valor == ""){
+        valor = 0;
+    }
+    valor = raiz(valor);
+    mostrar_resultado();
+}
+
+function zerar(){
+    if(desligada) return;
+    a = "";
+    b = "";
+    valor = "0";
+    executar = "";
+    mostrar_resultado();
+    valor = "";
+}
+
+function operacao(op){
+    if(desligada) return;
+    executar = op;
+    a = valor;
+    valor = "";
+}
+
+
+function digitando(tecla) {
+    if(desligada) return;
+    if (tecla == ".") {
+        if (!temPonto) {
+            valor = valor + tecla;
+            mostrar_resultado();
+            temPonto = true;
+        }
+        return
+    }
+    valor = valor + tecla;
+    mostrar_resultado();
 }
